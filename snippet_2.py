14292b1c44c1a268f67ba74c7e059a30cc99ba7e
Abraham Shimekt <abrahamshimekt4096@gmail.com>
2023-03-27 20:59:10 +0300

    code review for snippet 2

--- snippet_2.py
+++ snippet_2.py
@@ -1,22 +1,20 @@
 def is_valid_parenthesis(s):
-    dc = {
-        '(': ')',
-          "{": "}",
-         '[': ']'
-
-    }
+    # should be in one line for readability
+    # dc should be changed to parenthesises
+    parenthesises = {'(': ')',"{": "}",'[': ']'}
     stack = []
-    for i in s:
-        if i in dc.keys(): stack.append(i)
+    # i should be char 
+    for char in s:
+        # two lines are good for readablity instead
+        if char in parenthesises.keys():
+            stack.append(char)
         else:
             if len(stack) == 0:
                 return False
             
-
-            op = stack.pop()
-            if dc[op] != i:
+            # op should be changed to parentheses to make it descriptive
+            parentesis = stack.pop()
+            if parenthesises[parentesis] != char:
                 return False
-    if len(stack) != 0:
-        return False
-    
-    return True
+    # unneccessary return statements should removed for readabilty
+    return len(stack)==0
